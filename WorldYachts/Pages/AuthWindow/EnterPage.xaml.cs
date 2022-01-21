298f8af1d5712c71bc447130cb8d63374a9b5340
Алина А. Отыргашева <alinaviot@KHPK.RU>
2022-01-21 15:31:42 +0700

    210122

--- WorldYachts/Pages/AuthWindow/EnterPage.xaml.cs
+++ WorldYachts/Pages/AuthWindow/EnterPage.xaml.cs
@@ -20,9 +20,75 @@ namespace WorldYachts.Pages.AuthWindow
     /// </summary>
     public partial class EnterPage : Page
     {
+        bdWorldYachtsEntities context = new bdWorldYachtsEntities();
+
+        public string warnintText;
+        public int countMistakes = 0;
+        public int timeToBlick = 15;
         public EnterPage()
         {
             InitializeComponent();
+            txtWarning.Text = warnintText;
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            AppUser user = checkLoginPassword(txtLogin.Text, txtPass.Password);
+            if (countMistakes < 3)
+            {
+                if (user != null)
+                {
+                    if (checkPasswordDate(user))
+                    {
+                        AdminWindow window = new AdminWindow(user);
+                        window.Activate();
+                    }
+                }
+                else countMistakes++;
+            }
+            else blockEnter();
+        }
+
+        private void btnNewUser_Click(object sender, RoutedEventArgs e)
+        {
+            FrameManager.usedFrame.Navigate(new AddUserPage());
+        }
+        
+        AppUser checkLoginPassword(string login, string password)
+        {
+            var user = from x in context.AppUser
+                           where x.Login == login && x.Password == password
+                           select x;
+
+            if (user.Count() != 0)
+            {
+                return user.First();
+            }
+
+            return null;
+        }
+        bool checkPasswordDate(AppUser user)
+        {
+            if ((user.DateLastVisit.Date - DateTime.Now.Date).Days >= 14)
+            {
+                goToChangePassword(user);
+                return false;
+            }
+            if ((user.DateLastVisit.Date - DateTime.Now.Date).Days >= 31)
+            {
+                warnintText = "Ваша учётная запись заблокирована ввиду отсутствия посещений в течение месяца. Для продолжения работы попросите администратора открыть доступ к ней.";
+                return false;
+            }
+            return true;
+        }
+        void goToChangePassword(AppUser user)
+        {
+            FrameManager.usedFrame.Navigate(new ChangePassPage(user));
+        }
+        
+        void blockEnter()
+        {
+            warnintText = "Вы совершили слишком много ошибок. Вход заблокирован на " + timeToBlick + " секунд";
         }
     }
 }
